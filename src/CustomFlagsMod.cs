e9343611a81fca1c03e25181f712fa991ffa8230
adamsong <adamsong@users.noreply.github.com>
2023-03-15 20:23:07 -0400

    Adds API for adding custom flags

--- src/CustomFlagsMod.cs
+++ src/CustomFlagsMod.cs
@@ -1,10 +1,12 @@
 using System.Collections.Generic;
+using System.Data;
 using System.IO;
 using BepInEx;
 using KSP.Game;
 using KSP.Modding;
 using SpaceWarp;
 using SpaceWarp.API.Mods;
+using UniLinq;
 using UnityEngine;
 using UnityEngine.AddressableAssets;
 using UnityEngine.AddressableAssets.ResourceLocators;
@@ -20,45 +22,88 @@ namespace CustomFlags
 		public Sprite Sprite;
 	}
 	
-	[BepInPlugin("us.adamsogm.CustomFlags", "CustomFlags", "2.0.0")]
+	
+	
+	[BepInPlugin(ModGuid, ModName, ModVer)]
 	[BepInDependency(SpaceWarpPlugin.ModGuid, SpaceWarpPlugin.ModVer)]
 	public class CustomFlagsMod : BaseSpaceWarpPlugin
 	{
-		public static readonly Dictionary<string, FlagResource> Flags = new Dictionary<string, FlagResource>();
-		
-		private AgencyFlagsData _agencyFlagsData; 
+		public const string ModGuid = "us.adamsogm.CustomFlags";
+		public const string ModVer = "3.0.0";
+		public const string ModName = "CustomFlags";
+		public static string Path { get; private set; }
+
+		public override void OnPreInitialized()
+		{
+			Path = PluginFolderPath;
+		}
+
+		internal static readonly Dictionary<string, FlagResource> Flags = new();
+		private List<ResourceLocationData> _locationData = new();
+
+		private AgencyFlagsData _agencyFlagsData;
+
 		public override void OnInitialized()
 		{
-			var foundAssets = Resources.FindObjectsOfTypeAll<AgencyFlagsData>();
-			_agencyFlagsData = foundAssets[0];
+			_agencyFlagsData = Resources.FindObjectsOfTypeAll<AgencyFlagsData>()[0];
+			
 			var dirInfo = new DirectoryInfo("flags");
 			Logger.LogInfo($"Loading {dirInfo.GetFiles("*.png").Length} new flags.");
-			var locationData = new List<ResourceLocationData>();
-			foreach (var file in dirInfo.GetFiles("*.png"))
+			foreach (var fileInfo in dirInfo.GetFiles("*.png"))
 			{
-				var bytes = File.ReadAllBytes(file.FullName);
-				var texture = new Texture2D(512, 320, TextureFormat.RGB24, false);
-				texture.LoadImage(bytes);
-				var sprite = Sprite.Create(texture, new Rect(0, 0, texture.width, texture.height),
-					new Vector2(0.5f, 0.5f));
-				sprite.name = $"custom-flags-file-{file.Name}";
-				var newFlag = new AgencyFlag
-				{
-					FlagName = file.Name,
-					FlagIcon = sprite,
-					PrimaryColor = Color.black,
-					SecondaryColor = Color.black
-				};
-				Logger.LogInfo($"Loaded new flag {file.Name}");
-				_agencyFlagsData.Flags.Add(newFlag);
-				Flags.Add(sprite.name, new FlagResource
-				{
-					Texture = texture,
-					Sprite = sprite
-				});
-				locationData.Add(new ResourceLocationData(new []{sprite.name}, sprite.name, typeof(FlagResourceProvider), typeof(Sprite)));
+				Logger.LogInfo($"Loading flag from {fileInfo.FullName}");
+				LoadCustomFlag(fileInfo);
 			}
-			Addressables.AddResourceLocator(new ResourceLocationMap("custom-flags-location-map", locationData));
+			
+			GameManager.Instance.Game.Assets.LoadByLabel("custom_flag",
+				sprite => LoadCustomFlag(sprite, false), delegate(IList<Sprite> _)
+				{});
+		}
+
+		public void LoadCustomFlag(string file)
+		{
+			LoadCustomFlag(new FileInfo(file));
+		}
+		
+		public void LoadCustomFlag(FileInfo fileInfo)
+		{
+			var bytes = File.ReadAllBytes(fileInfo.FullName);
+			var texture = new Texture2D(512, 320, TextureFormat.RGB24, false);
+			texture.LoadImage(bytes);
+			var sprite = Sprite.Create(texture, new Rect(0, 0, texture.width, texture.height), new Vector2(0.5f, 0.5f));
+			sprite.name = $"custom-flags-file-{fileInfo.Name}";
+			LoadCustomFlag(sprite);
+		}
+
+		public void LoadCustomFlag(Sprite sprite, bool addAddress = true)
+		{
+			if (Flags.ContainsKey(sprite.name))
+			{
+				Logger.LogError($"Flag sprites with duplicate names: {sprite.name}. Only one will load.");
+				return;
+			}
+
+			var newFlag = new AgencyFlag
+			{
+				FlagName = sprite.name,
+				FlagIcon = sprite,
+				PrimaryColor = Color.black,
+				SecondaryColor = Color.black
+			};
+			_agencyFlagsData.Flags.Add(newFlag);
+			Logger.LogInfo($"Loaded flag {sprite.name}");
+			if (!addAddress) return;
+			Flags.Add(sprite.name, new FlagResource
+			{
+				Texture = sprite.texture,
+				Sprite = sprite
+			});
+			_locationData.Add(new ResourceLocationData(new []{sprite.name}, sprite.name, typeof(FlagResourceProvider), typeof(Sprite)));
+		}
+
+		public override void OnPostInitialized()
+		{
+			Addressables.AddResourceLocator(new ResourceLocationMap("custom-flags-location-map", _locationData));
 			Addressables.ResourceManager.ResourceProviders.Add(new FlagResourceProvider());
 		}
 	}
